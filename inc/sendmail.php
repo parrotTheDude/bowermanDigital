436cc2cec70eec147108fb2ccdac0591206bddc8
Jacob <program.acquaint533@passinbox.com>
2025-01-03 14:28:23 +1100

    email functionality works now

--- inc/sendmail.php
+++ inc/sendmail.php
@@ -2,7 +2,12 @@
   // Import the Postmark Client Class:
   require_once('./vendor/autoload.php');
   require_once('inc/variables.php');
+  require_once('inc/db-connect.php');
+
+  db_connect();
+
   use Postmark\PostmarkClient;
+  use Postmark\Models\PostmarkAttachment;
   $status = false;
 
   if(isset($_POST['submit'])) {
@@ -10,14 +15,26 @@
     $email = str_replace(' ', '', strtolower($_POST['email']));
     $phone = preg_replace("/[^0-9]/", '', $_POST['phone']);
     $message = $_POST['message'];
+    $dateCreated = date("Y-m-d H:i:s", time());
+    $hash = $name . $dateCreated;
+    $id = hash('sha256', $hash);
+    $emailValid = 0;
+
+    $sql_statement = "
+    INSERT INTO users (id, name, email, phone, message, emailValid, dateCreated) 
+    VALUE ('$id', '$name', '$email', '$phone', '$message', '$emailValid', '$dateCreated')";
+
+    $users = $db_link->query($sql_statement) or die($db_link->error);
 
     $postmarkToken = POSTMARK_TOKEN;
 
     if (preg_match("/^[a-zA-Z-' ]*$/",$name)) {
       if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $client = new PostmarkClient($postmarkToken);
+
+        $templateId = 38566274;
         $fromEmail = "hello@bowermandigital.com";
-        $toEmail = "hello@bowermandigital.com";
+        $toEmail = $email;
         $subject = $name . " wants to get in contact.";
         $htmlBody = "<strong>Hello!</strong> My name is " . $name . ". <br /><br />Email: " . $email . "<br />Phone: " . $phone . "<br /><br />Message:<br />" . $message;
         $textBody = "Hello! My name is " . $name  . ".Email: " . $email . "Phone: " . $phone . "Message:" . $message;;
@@ -26,7 +43,33 @@
         $trackLinks = "None";
         $messageStream = "outbound";
 
-        // Send an email:
+        // Send an email to client to confirm:
+        $sendResult = $client->sendEmailWithTemplate(
+          $fromEmail,
+          $toEmail,
+          $templateId,
+          ["product_url" => "https://bowermandigital.com", 
+          "product_name" => "Bowerman Digital",
+          "name" => $name, 
+          "sender_name" => "Jacob",
+          "company_name" => "Sydney",
+          "company_address" => "Australia"],
+          true, // Inline CSS
+          "enquiry-confirmation", // Tag
+          true, // Track opens
+          NULL, // Reply To
+          NULL, // CC
+          NULL, // BCC
+          NULL, // Header array
+          NULL, // Attachment array
+          NULL, // Track links
+          NULL, // Metadata array
+          NULL // Message stream
+        );
+
+        $toEmail = "hello@bowermandigital.com";
+
+        // Send an email to me about contact information
         $sendResult = $client->sendEmail(
           $fromEmail,
           $toEmail,
