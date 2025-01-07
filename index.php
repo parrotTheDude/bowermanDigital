c7bf7807b98eafd5a11e63f2cd1db70ec971a085
Jacob <program.acquaint533@passinbox.com>
2025-01-07 18:47:53 +1100

    edits to index page

--- index.php
+++ index.php
@@ -23,7 +23,9 @@
       <!-- Hero Div -->
       <section class="hero">
         <section class="initalImgMobileContainer">
-          <img class="initalImgMobile" src="img/mockups/evie-mobile2.webp" alt="Evie Bowerman portfolio website">
+          <a href="https://bowermandigital.com/evie-graphic-design">
+            <img class="initalImgMobile" src="img/mockups/evie-mobile2.webp" alt="Evie Bowerman portfolio website">
+          </a>
         </section>
 
         <section class="heroContainer">
@@ -40,16 +42,17 @@
         </section>
 
         <section class="portfolioSlide">
-          <img class="mobileMockEvie" src="img/mockups/evie-mobile.webp" alt="eviebowerman.com iPhone mockup">
+          <a href="https://bowermandigital.com/evie-graphic-design">
+            <img class="mobileMockEvie" src="img/mockups/evie-mobile.webp" alt="eviebowerman.com iPhone mockup"></a>
         </section>
       </section>
     </section>
 
 
     <!-- Portfilio Section -->
-    
     <section class="portfolioIndexContainer ">
       <h2>Our work so far</h2>
+      <p>Something something... dark side</p>
       <section class="contentContainer glow-on-hover">
         <section class="imgContentContainer">
           <img src="img/tdac.webp" alt="TDAC Promotional Picture" class="imgClassContentContainer">
@@ -79,7 +82,7 @@
     </section>
 
 
-    <!-- About Section -->
+    <!-- Services Section -->
     <section class="aboutIndexContainer">
       <!-- Fancy Background -->
       <section class="el" id="portfolioIndex">
@@ -104,17 +107,56 @@
         </aside>
       </section>
       <section class="btnContainer" id="aboutBtn">
-          <a href="https://bowermandigital.com/about">
-            <div class="glow-on-hover"><p>Learn about our story</p></div>
+          <a href="https://bowermandigital.com/services">
+            <div class="glow-on-hover"><p>Learn about our services</p></div>
           </a>
         </section>
       </section>
     </section>
 
+    <!-- Blog Section -->
+    <!-- <section class="portfolioIndexContainer ">
+      <h2>Check our our latest blogs!</h2>
+      <p>Bloggy bloggy blog blog</p>
+      <section class="contentContainer glow-on-hover">
+        <section class="imgContentContainer">
+          <img src="img/tdac.webp" alt="TDAC Promotional Picture" class="imgClassContentContainer">
+        </section>
+        <section class="titleContentContainer">
+          <h3>That Disability Adventure Company</h3>
+        </section>
+        <section class="btnContainer" id="aboutBtn">
+          <a href="https://bowermandigital.com/that-disability-adventure-company">
+            <div class="glow-on-hover"><p>TDAC's Journey</p></div>
+          </a>
+        </section>
+      </section>
+      <section class="contentContainer glow-on-hover">
+        <section class="imgContentContainer">
+          <img src="img/evie.webp" alt="Evie Bowerman Promotional Picture" class="imgClassContentContainer">
+        </section>
+        <section class="titleContentContainer">
+          <h3>Evie's Graphics Portfolio</h3>
+        </section>
+        <section class="btnContainer" id="aboutBtn">
+          <a href="https://bowermandigital.com/evie-graphic-design">
+            <div class="glow-on-hover"><p>Evie's Journey</p></div>
+          </a>
+        </section>
+      </section>
+    </section> -->
+
     <section class="contactIndexContainer">
       <section class="contactIndexContainerTitle">
         <h2>Get in touch</h2>
-        <p class="subHeading">Head over to our contact page to find out more!</p>
+        <p class="subHeading">What are you waiting for? Click the button below to get to our contact form!</p>
+        <img src="icons/arrow.svg" class="downArrow" alt="Arrow pointing to the contact form">
+        <section class="btnContainer" id="aboutBtn">
+          <a href="https://bowermandigital.com/contact">
+            <div class="glow-on-hover"><p>Contact us today</p></div>
+          </a>
+        </section>
+      </section>
       </section>
       
     </section>
